4a2c50e0dfa54953cf97e57892233538b9f75630
Kyle Bricker <57820561+kbricker8@users.noreply.github.com>
2023-01-09 18:18:09 -0700

    Modified script.py to download itself

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import requests
 
 print(requests.__version__)
-
 print(requests.get('http://www.google.com/'))
+r = requests.get('https://raw.githubusercontent.com/kbricker8/Lab1/main/script.py')
+
+print(r.text)
